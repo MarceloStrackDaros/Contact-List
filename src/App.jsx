dab6fd7d7a778fb61e14d3b3870f52aed6586d3f
Marcelo Daros <marcelo.sdaros@gmail.com>
2022-05-30 20:38:01 -0300

    begin of add contacts form

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,50 @@ function App() {
   return (
     <div className={style.siteContainer}>
       <header className={style.siteHeader}>Bem Vindo(a) { name }!</header>
-      <section className={style.personalData}>
-        <h1 className={style.basicInfo}>Seus Dados:</h1>
-        <img src={picture} alt={"profile"} />
-        <p>Nome: {name}</p>
-        <p>Usuário: {user}</p>
-      </section>
+      <main className={style.siteContent}>
+        <section className={style.personalData}>
+          <h2 className={style.sectionTitle}>Seus Dados:</h2>
+          <div className={style.dataWrapper}>
+            <img src={picture} alt={"profile"} />
+            <div className="nameWrapper">
+              <p>Nome: {name}</p>
+              <p>Usuário: {user}</p>
+            </div>
+          </div>
+        </section>
+        <form className={style.addContactForm}>
+          <h2 className={style.sectionTitle}>Inserir novo contato:</h2>
+          <fieldset>
+            <label htmlFor="name">Nome: </label>
+            <input type="text" name="name" required/>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="surname">Apelido: </label>
+            <input type="text" name="surname" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="telephone">Telefones: </label>
+            <input type="tel" name="telephone" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="address">Endereço: </label>
+            <input type="text" name="address" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="notes">Notas: </label>
+            <input type="text" name="notes" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="picture">Foto: </label>
+            <input type="file" name="picture" />
+          </fieldset>
+        </form>
+      </main>
+
       <form className={style.form}>
       </form>
     </div>
